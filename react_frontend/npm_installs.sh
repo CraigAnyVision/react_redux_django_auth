3d6e3776a6cfcf5c41307b5ab8fa8b5b448a98ba
craig <sgccarey@gmail.com>
2017-11-01 21:51:21 +0000

    Renamed frontend project

---
+++ react_frontend/npm_installs.sh
@@ -0,0 +1,8 @@
+npm install --save redux react-redux
+npm install --save react-router-redux@next history
+npm install --save redux-persist@5.0.0-beta.7
+npm install --save redux-persist-transform-filter
+npm install --save redux-api-middleware@2.0.0-beta.3
+npm install --save jwt-decode
+npm install --save reactstrap react-transition-group@1.1.2
+npm install --save bootstrap@4.0.0-beta
--- react_frontend/src/store.js
+++ react_frontend/src/store.js
@@ -1,6 +1,6 @@
 import storage from 'redux-persist/es/storage'
 import { applyMiddleware, createStore } from 'redux'
-import { createFilter   } from 'redux-persist-transform-filter';
+import { createFilter } from 'redux-persist-transform-filter';
 import { persistReducer, persistStore } from 'redux-persist'
 import { routerMiddleware } from 'react-router-redux'
 
